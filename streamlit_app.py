337c0ddfb4b3b1a2aed285f02e98a43bea4fab77
Jorge Ramos <121762884+jorgeluisramosangulo@users.noreply.github.com>
2025-04-07 14:24:10 +0100

    read data from dataprofessor
    read data from dataprofessor

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,10 @@ st.title('🤖 Machine Learning App')
 
 st.info('This is app builds a machine learning model!')
 
-# with st.expander('Data'):
-#   st.write('**Raw data**')
-#   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-#   df
+with st.expander('Data'):
+   st.write('**Raw data**')
+   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+   df
 
 #   st.write('**X**')
 #   X_raw = df.drop('species', axis=1)
