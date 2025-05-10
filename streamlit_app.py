c091621f2c7e9def68cdf9d891ce6d4074d1999b
Jorge Ramos <121762884+jorgeluisramosangulo@users.noreply.github.com>
2025-05-10 15:21:09 +0100

    svm 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ from sklearn.metrics import classification_report, accuracy_score
 ######################################    Presentation   #################################################################
 ##########################################################################################################################
 
-st.title("🤖 Binary Classification App")
+st.title("🤖 Binary Classification Appp")
 st.info("This app builds a binary classification model!")
 
 
