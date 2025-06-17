0f89929c2ce0b63d66469c1fcd582dd847e97086
Jorge Ramos <121762884+jorgeluisramosangulo@users.noreply.github.com>
2025-06-17 15:57:32 +0100

    RRRRRRRRRRRRR

--- streamlit_app.py
+++ streamlit_app.py
@@ -332,7 +332,7 @@ if df is not None:
 #################################        Target Selection    #############################################################
 ##########################################################################################################################
 
-    st.markdown("### 🎯 Step 2: Select Target Columrrrrrrrrrrrrrn")
+    st.markdown("### 🎯 Step 2: Select Target ColumrRRRRRRRRRRRRRRRn")
 
     # Initialize session state variables if not already set
     if "target_confirmed" not in st.session_state:
